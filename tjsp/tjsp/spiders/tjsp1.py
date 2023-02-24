18ffefaf1a4b39e931bb02317f33253e6a10dc8e
Dalerem <dalerem2022@gmail.com>
2023-02-24 17:46:57 -0300

    beta9

--- tjsp/tjsp/spiders/tjsp1.py
+++ tjsp/tjsp/spiders/tjsp1.py
@@ -4,8 +4,10 @@ class Tjsp1Spider(scrapy.Spider):
     name = "tjsp1"
     start_urls = ["https://esaj.tjsp.jus.br/esaj/portal.do?servico=190090"]
 
+    # Função resposta
     def parse(self, response):
         for consultas in response.css('.esajTabelaServico'):
+            # Coletar os titulos
             yield {
                 'titulos': consultas.css('.esajCelulaDescricaoServicos a::text').get()
             }
